0e06ab291cbf48277f32c51e0e56b45d7a5263b6
SkyMasterROG <modder1980@gmail.com>
2024-03-19 20:18:03 +0100

    clear syntax 'update.sh'

--- update.sh
+++ update.sh
@@ -2,15 +2,14 @@
 
 GS_LOCAL_REPO='/etc/gravity-sync/.gs'
 
-    if [ -f "${GS_LOCAL_REPO}/dev" ]; then
-        source ${GS_LOCAL_REPO}/dev
-    else
-        BRANCH='origin/master'
-    fi
-    
-    if [ "$BRANCH" != "origin/master" ]; then
-        echo -e "Pulling from ${BRANCH}"
-    fi
-    
-    (cd ${GS_LOCAL_REPO}; sudo git fetch --all; sudo git reset --hard ${BRANCH}; sudo cp gravity-sync /usr/local/bin; sudo git clean -fq)
+if [ -f "${GS_LOCAL_REPO}/dev" ]; then
+    source ${GS_LOCAL_REPO}/dev
+else
+    BRANCH='origin/master'
+fi
 
+if [ "$BRANCH" != "origin/master" ]; then
+    echo -e "Pulling from ${BRANCH}"
+fi
+
+(cd ${GS_LOCAL_REPO}; sudo git fetch --all; sudo git reset --hard ${BRANCH}; sudo cp gravity-sync /usr/local/bin; sudo git clean -fq)
